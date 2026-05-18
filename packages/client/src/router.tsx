2c617baa4cc3678cd6e6f65d7e05d6dea412169b
Kaito <74711118+sc30gsw@users.noreply.github.com>
2026-05-18 12:47:26 +0900

    Refactor routing structure to include authenticated routes; updated route tree to nest Home and Live routes under AuthenticatedRoute.

--- packages/client/src/router.tsx
+++ packages/client/src/router.tsx
@@ -1,12 +1,17 @@
 import { createRouter } from "@tanstack/react-router";
 
+import { Route as AuthenticatedRoute } from "~/routes/authenticated";
 import { Route as HomeRoute } from "~/routes/home";
 import { Route as LiveRoute } from "~/routes/live";
 import { Route as RootRoute } from "~/routes/root";
 import { Route as SignInRoute } from "~/routes/sign-in";
 import { Route as SignUpRoute } from "~/routes/sign-up";
 
-const routeTree = RootRoute.addChildren([HomeRoute, LiveRoute, SignInRoute, SignUpRoute]);
+const routeTree = RootRoute.addChildren([
+  AuthenticatedRoute.addChildren([HomeRoute, LiveRoute]),
+  SignInRoute,
+  SignUpRoute,
+]);
 
 export const router = createRouter({ routeTree });
 
